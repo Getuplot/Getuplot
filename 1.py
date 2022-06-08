168f38475801d8d3f4e93f27b3b435fcfe54abe9
Getuplot <106401928+Getuplot@users.noreply.github.com>
2022-06-08 14:28:22 +0200

    Update 1.py
    Opravená verze programu

--- 1.py
+++ 1.py
@@ -1,8 +1,12 @@
-while True:
-    from random import randrange
-    x = randrange(1,11)
-    print(x)
-    if x==10:
-        print("Bylo vybráno nejvyšší číslo.")
-        break
-input()
+import random
+randomlist = []
+maxcisel = 5
+for i in range(maxcisel):
+   n = random.randint(1,30)
+   randomlist.append(n)
+print("Vygenerované pole hodnot:", randomlist)
+max_value = max(randomlist)
+max_index = randomlist.index(max_value)
+print("Index, kde se nachází nejvyšší hodnota:", max_index)
+randomlist[max_index], randomlist[maxcisel-1] = randomlist[maxcisel-1], randomlist[max_index]
+print("Nové pole: Vyměněna největší hodnota s hodnotou, která má největší index", randomlist)
